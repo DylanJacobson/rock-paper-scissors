ef04c5ee70558f200336c050d93fd3d22e7ee71a
Dylan Jacobson <dylanrobertjacobson@gmail.com>
2020-11-17 00:19:11 -0700

    Adds keyboard shorcut to instructions

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
+let replaceSpan = document.getElementById("replace");
+let deleteIfNoID = document.getElementById("deleteIfNoID");
+let browser = function(){
+  var ua= navigator.userAgent, tem,
+  M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if(/trident/i.test(M[1])){
+      tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+      return 'IE '+(tem[1] || '');
+  }
+  if(M[1]=== 'Chrome'){
+      tem= ua.match(/\b(OPR|Edge?)\/(\d+)/);
+      if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera').replace('Edg ', 'Edge ');            
+  }
+  M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+  return M.join(' ');
+}();
+
+let controlKey = "Control"
+let altKey = "Shift"
+if (navigator.userAgent.indexOf("Mac") != -1) {
+  controlKey = "Command"
+  altKey = "Option"
+}
+
+if (browser.includes("Chrome") || browser.includes("Edge")) {
+  replaceSpan.textContent = `${controlKey} + ${altKey} + J`;
+} else if (browser.includes("Firefox")) {
+  replaceSpan.textContent = `${controlKey} + ${altKey} + K`;
+} else if (browser.includes("Safari")) {
+  replaceSpan.textContent = `${controlKey} + ${altKey} + C`;
+} else {
+  deleteIfNoID.textContent = "";
+}
+
 function computerMove() {
   let moves = ["rock", "paper", "scissors"];
-
   return moves[Math.floor(Math.random()*moves.length)];
 }
 
