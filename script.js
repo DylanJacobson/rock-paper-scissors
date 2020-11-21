9dcd78aef8e9a30747066f249f5957e351601199
Dylan Jacobson <dylanrobertjacobson@gmail.com>
2020-11-20 21:12:04 -0700

    Simplifies and separates functions

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const resultsDisplay = document.getElementById("results");
+const resultDisplay = document.getElementById("results");
+const scoreDisplay = document.getElementById("score");
 const buttons = document.querySelectorAll("button");
-let gamesPlayed = 0;
-let gamesWon = 0;
-let gamesLost = 0;
+let gameScore = {
+  player: 0,
+  computer: 0,
+  draw: 0,
+};
+
 
 const reloadLink = document.createElement('a');
 reloadLink.textContent = "Play again?";
@@ -10,54 +14,71 @@ reloadLink.addEventListener('click', function () {
   location.reload();
 });
 
-let replaceSpan = document.getElementById("replace");
-function computerMove() {
+document.querySelectorAll('button').forEach(button => {
+  button.addEventListener('click', function () {
+    playRound(this.id);
+  });
+});
+
+function playRound(playerChoice) {
+  const computerChoice = computerChoiceMaker();
+
+  const roundResult = compareChoices(playerChoice, computerChoice);
+
+  if (roundResult === 'player') {
+    gameScore.player++;
+  } else if (roundResult === 'computer') {
+    gameScore.computer++;
+  } else {
+    gameScore.draw++;
+  }
+
+  if (gameScore.player >= 5) {
+    endGame("player");
+  } else if (gameScore.computer >= 5) {
+    endGame("computer");
+  } else {
+    renderRound(roundResult);
+  }
+}
+
+function computerChoiceMaker() {
   let moves = ["rock", "paper", "scissors"];
   return moves[Math.floor(Math.random()*moves.length)];
 }
 
-function resultText(result, playerChoice, computerChoice) {
-  if (result == true) {
-    return "You win this round, " + playerChoice + " beats " + computerChoice + "!";
-  } else if (result == false) {
-    return "You lose this round, " + computerChoice + " beats " + playerChoice + "!";
-  }
-}
+function compareChoices(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) return "draw";
 
-function playRound(playerChoice) {
-  let computerChoice = computerMove();
-  playerChoice = playerChoice.toLowerCase();
-  if (playerChoice === computerChoice) return "It's a draw! You both chose " + playerChoice + ".";
-
-  if (playerChoice === 'rock' && computerChoice === 'scissors' ||
-      playerChoice === 'paper' && computerChoice === 'rock' ||
-      playerChoice === 'scissors' && computerChoice === 'paper') {
-        gamesWon += 1;
-        return resultText(true, playerChoice, computerChoice); 
+  if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')) {
+        return "player"; 
       } else {
-        gamesLost += 1;
-        return resultText(false, playerChoice, computerChoice);
+        return "computer";
       }
 }
 
-for (i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function (event) {
-    gamesPlayed += 1;
-    let result = playRound(this.textContent);
-    if (gamesWon < 5 && gamesLost < 5) {
-      resultsDisplay.textContent = result + " You've won " + gamesWon + " out of " + gamesPlayed + " games!";
-    } else if (gamesWon >= 5) {
-      resultsDisplay.textContent = result + " You've won 5 games, you're the champion! ";
-      resultsDisplay.appendChild(reloadLink);
-      document.querySelectorAll('button').forEach(button => {
-        button.disabled = true;
-      });
-    } else if (gamesLost >= 5) {
-      resultsDisplay.textContent = result + " I've won 5 games, I'm the champion! ";
-      resultsDisplay.appendChild(reloadLink);
-      document.querySelectorAll('button').forEach(button => {
-        button.disabled = true;
-      });
-    }
+function renderRound(roundResult) {
+  if (roundResult === "draw") {
+    resultDisplay.textContent = "It's a draw!"
+  } else {
+    resultDisplay.textContent = `The ${roundResult} has won this round!`;
+  }
+  updateScoreDisplay();
+}
+
+function updateScoreDisplay() {
+  scoreDisplay.textContent = `Player: ${gameScore.player} - Computer: ${gameScore.computer} - Draws: ${gameScore.draw}`
+}
+
+function endGame(champion) {
+  resultDisplay.textContent = `The ${champion} has won 5 rounds, they are the champion! `
+  resultDisplay.appendChild(reloadLink);
+
+  updateScoreDisplay();
+
+  document.querySelectorAll('button').forEach(button => {
+    button.disabled = true;
   });
 }
